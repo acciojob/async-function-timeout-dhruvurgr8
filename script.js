29a0733660d85fe4049eba645747e4b318dcd789
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-22 14:17:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const btn = document.querySelector('#btn');
+const showContent = document.querySelector('#output');
+
+btn.addEventListener('click',()=>{
+	const text = document.querySelector('#text').value;
+const num = document.querySelector('#delay').value;
+	function createPromise(text,num){
+		return new Promise((res,rej)=>{
+			if(text && num){
+				res(`${text}-${num}`)
+			}else{
+				rej("error occured")
+			}
+		})
+	}
+	async function getPromise(){
+		const res = await createPromise(text,num);
+		showContent.textContent = res;
+	}
+	getPromise();
+})
